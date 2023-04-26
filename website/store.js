aa367171a42369c08f2aadca54b3accc3540aaf6
laurenggraham <lggraham@usc.edu>
2023-04-26 13:52:19 -0700

    Updates to Store/Customization

---
+++ website/store.js
@@ -0,0 +1,27 @@
+function updateProduct() {
+    // Get selected options
+    var color = document.getElementById("ring-color").value;
+    var material = document.getElementById("ring-material").value;
+    var size = document.getElementById("ring-size").value;
+  
+    // Update image and price based on selected options
+    var image = "White-Resin.PNG";
+    var price = "$20";
+    if (color === "Light" && material === "Resin") {
+      image = "White-Resin.PNG";
+      price = "$20";
+    } else if (color === "Dark" && material === "Resin") {
+      image = "Black-Resin.PNG";
+      price = "$25";
+    } else if (color === "Light" && material === "Titanium") {
+      image = "White-Metal.PNG";
+      price = "$70";
+    }
+ else if (color === "Dark" && material === "Titanium") {
+    image = "Black-Metal.PNG";
+    price = "$75";
+  }
+    document.getElementById("ring-image").src = image;
+    document.getElementById("ring-price").innerHTML = price;
+  }
+  
