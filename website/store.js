2a900cb9ba06528fad071686792e257f16d80898
tiluuta <tiluutan@gmail.com>
2023-04-27 12:47:29 -0700

    merged lauren's and tiffany's additions

--- website/store.js
+++ website/store.js
@@ -1,17 +1,14 @@
-$(document).ready(function() { //why wont this work!!!!!!!??????????
-    $("#addtocart").on("click", function(){
-        alert("this works")
-        var sizeselect = $("#ring-size").val();
-        if ( sizeselect == "default-size" ){
-            alert("Please select a size.");
-        }
-        else {
-            alert("Your item has been sccessfully added to your cart!");
-        }
-    });
-  });
-
-
+var items=[];
+var colors=[];
+var materials=[];
+var sizes=[];
+var prices=[];
+let i;
+if(localStorage.getItem("lastindex") != null){
+  i = localStorage.getItem("lastindex");
+}
+else
+  i=0;
 
 function updateProduct() {
     // Get selected options
@@ -20,26 +17,56 @@ function updateProduct() {
     var size = document.getElementById("ring-size").value;
   
     // Update image and price based on selected options
-    var image = "White-Resin.PNG";
-    var price = "$20";
-    if (color === "Light" && material === "Resin") {
+    let image = "White-Resin.PNG";
+    let price = "$20";
+
+    if (color === "Light") {
       image = "White-Resin.PNG";
       price = "$20";
-    } else if (color === "Dark" && material === "Resin") {
+    } if (color === "Dark" && material != "Titanium") {
       image = "Black-Resin.PNG";
       price = "$25";
-    } else if (color === "Light" && material === "Titanium") {
+    } if (color === "Light" && material === "Titanium") {
       image = "White-Metal.PNG";
       price = "$70";
     }
- else if (color === "Dark" && material === "Titanium") {
-    image = "Black-Metal.PNG";
-    price = "$75";
-  }
+    if (color === "Dark" && material === "Titanium") {
+      image = "Black-Metal.PNG";
+      price = "$75";
+    }
     document.getElementById("ring-image").src = image;
     document.getElementById("ring-price").innerHTML = price;
   }
 
+  function storeProduct(color, material, size, price){
+    if(localStorage.getItem("lastindex") != null){
+      i = localStorage.getItem("lastindex");
+    }
+    else
+      i=0;
+
+    console.log("i:" + i);
+    console.log("lastindex:" + localStorage.getItem("lastindex"));
+    items[i] = i;
+    colors[i] = color;
+    materials[i] = material;
+    sizes[i] = size;
+    prices[i] = price;
+    console.log(items[i]);
+    console.log(colors[i]);
+    console.log(sizes[i]);
+
+    localStorage.setItem("items[" + i + "]", items[i]);
+    localStorage.setItem("colors[" + i + "]", colors[i]);
+    localStorage.setItem("materials[" + i + "]", materials[i]);
+    localStorage.setItem("sizes[" + i + "]", sizes[i]);
+    localStorage.setItem("prices[" + i + "]", prices[i]);
+    localStorage.setItem("lastindex", parseFloat(i) + 1);
+
+    alert("Your item has been sccessfully added to your cart!");
+    
+  }
+  
 
 
   
