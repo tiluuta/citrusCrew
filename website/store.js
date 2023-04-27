714ecd78924a2684ff4b21961016dfaa7f4cb301
tiluuta <tiluutan@gmail.com>
2023-04-27 00:25:23 -0700

    CART

--- website/store.js
+++ website/store.js
@@ -1,8 +1,21 @@
+var items=[];
+var colors=[];
+var materials=[];
+var sizes=[];
+var prices=[];
+let i;
+if(localStorage.getItem("lastindex") != null){
+  i = localStorage.getItem("lastindex");
+}
+else
+  i=0;
+
+
 function updateProduct() {
     // Get selected options
-    var color = document.getElementById("ring-color").value;
-    var material = document.getElementById("ring-material").value;
-    var size = document.getElementById("ring-size").value;
+    const color = document.getElementById("ring-color").value;
+    const material = document.getElementById("ring-material").value;
+    const size = document.getElementById("ring-size").value;
   
     // Update image and price based on selected options
     var image = "White-Resin.PNG";
@@ -23,5 +36,32 @@ function updateProduct() {
   }
     document.getElementById("ring-image").src = image;
     document.getElementById("ring-price").innerHTML = price;
+  }
+
+  function storeProduct(color, material, size, price){
+    if(localStorage.getItem("lastindex") != null){
+      i = localStorage.getItem("lastindex");
+    }
+    else
+      i=0;
+
+    console.log("i:" + i);
+    console.log("lastindex:" + localStorage.getItem("lastindex"));
+    items.push(i);
+    colors.push(color);
+    materials.push(material);
+    sizes.push(size);
+    prices.push(price);
+    console.log(items[i]);
+    console.log(colors[i]);
+    console.log(sizes[i]);
+
+    localStorage.setItem("items[" + i + "]", items[i]);
+    localStorage.setItem("colors[" + i + "]", colors[i]);
+    localStorage.setItem("materials[" + i + "]", materials[i]);
+    localStorage.setItem("sizes[" + i + "]", sizes[i]);
+    localStorage.setItem("prices[" + i + "]", prices[i]);
+    localStorage.setItem("lastindex", parseFloat(i) + 1);
+    
   }
   
